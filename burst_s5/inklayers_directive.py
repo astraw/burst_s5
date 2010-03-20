6d90f0b924801b89609edad848b157b0187449fc
Andrew Straw <strawman@astraw.com>
2010-03-20 14:19:57 -0700

    inklayers directive: show when calling inkscape

--- burst_s5/inklayers_directive.py
+++ burst_s5/inklayers_directive.py
@@ -21,10 +21,14 @@ else:
 def get_stdout(cmd):
     """call a command and return the stdout, raising exception if error"""
     p = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    sys.stdout.write('calling "%s"...'%(' '.join(cmd)))
+    sys.stdout.flush()
     p.wait()
     if p.returncode != 0:
         raise RuntimeError('command "%s" failed with code %d'%(
                 ' '.join(cmd), p.returncode))
+    sys.stdout.write('OK\n')
+    sys.stdout.flush()
     return p.stdout.read()
 
 def get_width_height( fname ):
